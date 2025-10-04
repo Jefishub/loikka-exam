b7a702e51a7594182cb04713b50ef12a3f5337f8
Jere Manni <jere.manni@gmail.com>
2025-10-04 18:02:01 +0300

    navbar update, create context later

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+const Navbar = () => {
+    //Get values from context or props later
+    const companyName = "Ab Yritys Oy";
+    const totalAmount = "X XXX,XX€";
+    const orderStatus = "status";
 
-const Navbar = () => (
-    <nav className='w-full h-12 justify-between'    >
-        <h1 className='text-2xl font-bold text-center mt-4 '>
-            Ab Yritys Oy
-        </h1>
-    </nav>
-);
+    return (
+        <nav className='flex w-full h-20 justify-between px-4 pt-4'    >
+            <div className='text-2xl font-bold flex '>
+                {companyName}
+            </div>
+            <div className='flex gap-2'>
+                <div>
+                    <div className='text-2xl font-bold'>{totalAmount}</div>
+                    <div className='flex w-full justify-end'>{orderStatus}</div>
+                </div>
+                <div>
+                    <button className='h-14 w-20 bg-blue-500 text-white p-2 rounded'>Order</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
 
 export default Navbar;
